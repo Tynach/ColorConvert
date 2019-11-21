c736501a2c6ee55d127480954134506ce78c83cd
Tynach <tynach2@gmail.com>
2019-11-21 15:29:35 -0700

    Complete the renaming of color_space.

--- include/colormath/color_space.h
+++ include/colormath/color_space.h
@@ -5,8 +5,17 @@ using colorvec::vec3;
 using colorvec::mat3;
 
 namespace colormath {
-	class colorspace {
-		vec3 white_ref;
-		vec3 white_out;
+	class color_space {
+	public:
+		color_space() {}
+		virtual ~color_space() {}
+
+		virtual vec3 from_connection_space(vec3 c);
+		virtual vec3 to_connection_space(vec3 c);
+	};
+
+	class rgb_space {
+		mat3 primaries;
+		vec3 white_point;
 	};
 }
